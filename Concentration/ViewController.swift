8700352be6b9fb7c9ae1e9292341748031769e56
Tim <Tim@imac.local>
2018-10-07 20:04:14 -0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Tim on 10/7/18.
+//  Copyright © 2018 Timingila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

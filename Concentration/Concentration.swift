4f907e549efea20cf1e9d04022b33e677317e98c
Tim <Tim@imac.local>
2018-10-13 18:50:46 -0500

    Version 1.0.

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,47 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Tim on 10/10/18.
+//  Copyright © 2018 Timingila. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            // cards += [card, card]
+            cards.append(card)
+            cards.append(card)
+        }
+        cards.shuffle()
+    }
+}
